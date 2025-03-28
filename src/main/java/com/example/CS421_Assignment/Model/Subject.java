60e5600e4f744ff016c461dd52ece37f6413bc48
FazilChaos <fadhilimajala97@gmail.com>
2025-03-29 02:51:01 +0300

    Create Subject Entity

---
+++ src/main/java/com/example/CS421_Assignment/Model/Subject.java
@@ -0,0 +1,48 @@
+package com.example.CS421_Assignment.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int academicYear;
+
+    public Subject(){}
+
+    public Subject(Long id, String name, int academicYear) {
+        this.id = id;
+        this.name = name;
+        this.academicYear = academicYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAcademicYear() {
+        return academicYear;
+    }
+
+    public void setAcademicYear(int academicYear) {
+        this.academicYear = academicYear;
+    }
+}
