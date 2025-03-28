e52c2c304f763003e54bb2da40ae9217efc7c90a
FazilChaos <fadhilimajala97@gmail.com>
2025-03-29 02:44:33 +0300

    Add all files

---
+++ src/main/java/com/example/CS421_Assignment/Cs421AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.CS421_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs421AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs421AssignmentApplication.class, args);
+	}
+
+}
