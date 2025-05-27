223ec79ffbb83c968f24cf6668f329c96a61eb49
FazilChaos <fadhilimajala97@gmail.com>
2025-05-27 20:18:54 +0300

    Add get method for getting Students based on enrolledProgram

--- src/main/java/com/example/CS421_Assignment/Controller/StudentController.java
+++ src/main/java/com/example/CS421_Assignment/Controller/StudentController.java
@@ -4,6 +4,8 @@ import com.example.CS421_Assignment.Model.Student;
 import com.example.CS421_Assignment.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +16,13 @@ public class StudentController {
     private StudentRepository studentRepository;
 
     @GetMapping("/students")
-    public List<Student> getStudents() {
+    public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+
+    @GetMapping("/students/{enrolledProgram}")
+    public List<Student> getStudentByEnrolledProgram(@RequestParam String enrolledProgram) {
+        return studentRepository.findByEnrolledProgram(enrolledProgram);
     }
 }
