d82cb5f0d775c73ff5bb6973f28d36dd8f49e83a
FazilChaos <fadhilimajala97@gmail.com>
2025-05-27 20:22:41 +0300

    Add repository method for getting list of Students based on enrolledProgram

--- src/main/java/com/example/CS421_Assignment/Repository/StudentRepository.java
+++ src/main/java/com/example/CS421_Assignment/Repository/StudentRepository.java
@@ -3,5 +3,8 @@ package com.example.CS421_Assignment.Repository;
 import com.example.CS421_Assignment.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByEnrolledProgram(String enrolledProgram);
 }
